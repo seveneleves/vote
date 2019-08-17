1a6babe7096259acfc029b85adeffbaf0ec622c4
seveneleves <paullevent2014@u.northwestern.edu>
2019-08-17 16:23:00 +0200

    initial commit

---
+++ js/main.js
@@ -0,0 +1,27 @@
+document.getElementById('blockchain_button').addEventListener('click', function() {
+  document.querySelector('.div_to_animate').classList.add('animation');
+  setTimeout(function() {
+      document.querySelector('.div_to_animate').classList.remove('animation');
+    }, 2000);
+})
+
+document.getElementById('ia_button').addEventListener('click', function() {
+  document.querySelector('.div_to_animate').classList.add('animation');
+  setTimeout(function() {
+      document.querySelector('.div_to_animate').classList.remove('animation');
+    }, 2000);
+})
+
+document.getElementById('bdata_button').addEventListener('click', function() {
+  document.querySelector('.div_to_animate').classList.add('animation');
+  setTimeout(function() {
+      document.querySelector('.div_to_animate').classList.remove('animation');
+    }, 2000);
+})
+
+document.getElementById('sworkplace_button').addEventListener('click', function() {
+  document.querySelector('.div_to_animate').classList.add('animation');
+  setTimeout(function() {
+      document.querySelector('.div_to_animate').classList.remove('animation');
+    }, 2000);
+})
